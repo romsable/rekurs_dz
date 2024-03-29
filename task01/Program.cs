ea38b2179a155b77eb0a4d8a448ef954bf6f13da
sable <rom.sable@gmail.com>
2024-03-29 23:29:05 +0400

    add first task solution

--- task01/Program.cs
+++ task01/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string NatNumbers(int n, int m)
+{
+    if (m == n)
+    {
+        return Convert.ToString(m);
+    }
+    return m + " " + NatNumbers(n, m + 1);
+}
+
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(NatNumbers(N, M));
