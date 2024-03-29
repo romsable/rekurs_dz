ceff3f9c21e40c66c9eb4d8ab42cac9d98380bc1
sable <rom.sable@gmail.com>
2024-03-30 00:52:14 +0400

    add third task solution

--- task03/Program.cs
+++ task03/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+void PrintArrayFromEnd(int [] mas, int count=0)
+{
+     if (count == mas.Length) return;
+     PrintArrayFromEnd(mas,count + 1);
+     Console.Write($"{mas[count]} ");
+}
+
+int [] mas = {1, 2, 3, 4, 5};
+PrintArrayFromEnd(mas);
+
