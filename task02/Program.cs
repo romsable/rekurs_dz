3a4126c5fa3b943128ab105d194638b1b43549fd
sable <rom.sable@gmail.com>
2024-03-30 00:01:51 +0400

    add second task solution

--- task02/Program.cs
+++ task02/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int akkerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (m > 0 && n == 0) return akkerman(m - 1, 1);
+    if (m > 0 && n > 0) return akkerman(m - 1, akkerman(m, n - 1));
+    return 0;
+}
+
+Console.Write("Введите неотрицательное число m: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите неотрицательное число n: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(akkerman(m, n));
